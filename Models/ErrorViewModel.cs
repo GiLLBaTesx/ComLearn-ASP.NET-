1b169e1c3a6bb3a494d1b29d1c3d436c04a681c7
Jhon Vincent Colasi <106292961+GiLLBaTesx@users.noreply.github.com>
2023-04-04 20:50:58 +0800

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comlearnwebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
